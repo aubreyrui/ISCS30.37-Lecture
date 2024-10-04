a802dee789cd252b4617108d5308b66aa88060aa
Aubrey San Jose <mary.sanjose@student.ateneo.edu>
2024-10-04 13:21:33 +0800

    initial lecture stuff

---
+++ src/components/Clicker.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="increment">Click me</button>
+    <p>{{ props.label }} : {{ clicks }}</p>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const props = defineProps({
+    label: String
+  })
+
+    const clicks = ref(0)
+
+    function increment(evt) {
+        console.log(evt)
+        clicks.value++
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <input type="text" name="name" id="name" v-model="name">
+    <p>Name: {{ name }}</p>
+    <Clicker />
+    <ul>
+      <li v-for="item in items">
+  {{ item.message }}
+</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import Clicker from '@/components/Clicker.vue'
+  const name = ref('')
+  const items = ref([{ message: 'Foo' }, { message: 'Bar' }])
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
